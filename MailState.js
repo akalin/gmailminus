53e0c803eba3afa7955d371c0525fd737f40b026
Frederick Akalin <akalin@gmail.com>
2010-11-01 02:13:27 -0600

    added update method

--- MailState.js
+++ MailState.js
@@ -1,3 +1,10 @@
+MailState.prototype.update = function(email, count) {
+  this.email = email;
+  this.mail_count = count;
+  this.last_updated = null;
+  this.request_failures = 0;
+}
+
 MailState.prototype.clear = function() {
   this.email = null;
   this.mail_count = null;
