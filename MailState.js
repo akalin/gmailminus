23b160dc4f06452672a43d90f9adbba3907a1520
Frederick Akalin <akalin@gmail.com>
2010-11-01 02:11:03 -0600

    added clear function

--- MailState.js
+++ MailState.js
@@ -1,7 +1,11 @@
-function MailState(index) {
-  this.index = index;
+MailState.prototype.clear = function() {
   this.email = null;
   this.mail_count = null;
   this.last_updated = null;
   this.request_failures = 0;
+}
+
+function MailState(index) {
+  this.index = index;
+  this.clear();
 }
--- MailStateTest.js
+++ MailStateTest.js
@@ -7,4 +7,13 @@ describe('MailState', function () {
     expect(mail_state.last_updated).toEqual(null);
     expect(mail_state.request_failures).toEqual(0);
   });
+
+  it('cleared state is empty', function() {
+    var mail_state = new MailState(1);
+    expect(mail_state.index).toEqual(1);
+    expect(mail_state.email).toEqual(null);
+    expect(mail_state.mail_count).toEqual(null);
+    expect(mail_state.last_updated).toEqual(null);
+    expect(mail_state.request_failures).toEqual(0);
+  });
 });
