aeb048bd908033b648bc52f8d11e0ce0402b97ed
Frederick Akalin <akalin@gmail.com>
2010-11-01 20:39:11 -0700

    added feed test

--- MailStateTest.js
+++ MailStateTest.js
@@ -44,4 +44,24 @@ describe('MailState', function () {
     expect(mail_state.last_updated).toEqual(null);
     expect(mail_state.request_failures).toEqual(0);
   });
+
+  it('parse feed', function() {
+    var mail_state = new MailState(1);
+    var xml_text = '
+<feed xmlns="http://purl.org/atom/ns#" version="0.3">
+<title>Gmail - Inbox for test@foo.com</title>
+<tagline>New messages in your Gmail Inbox</tagline>
+<fullcount>533</fullcount>
+<link rel="alternate" href="http://mail.google.com/mail/u/0" type="text/html"/>
+<modified>2010-11-02T03:35:17Z</modified>
+</feed>
+';
+    var xml = (new DOMParser()).parseFromString(xml_text, "text/xml");
+    mail_state.parse_feed(xml);
+    expect(mail_state.index).toEqual(1);
+    expect(mail_state.email).toEqual('test@foo.com');
+    expect(mail_state.mail_count).toEqual(533);
+    expect(mail_state.last_updated).toEqual(null);
+    expect(mail_state.request_failures).toEqual(0);
+  });
 });
