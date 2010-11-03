bf69eb4e83c0ae858607eac96f8709a55f6ef027
Frederick Akalin <akalin@gmail.com>
2010-11-03 01:54:06 -0700

    coalesced functions

--- GmailAccountCheckerTest.js
+++ GmailAccountCheckerTest.js
@@ -4,18 +4,23 @@ FakeXMLHttpRequest.prototype.open = function() {}
 FakeXMLHttpRequest.prototype.send = function() {}
 
 describe('GmailAccountChecker', function () {
-  it('basic flow', function() {
+  var fakeReq;
+  var callbackSpy;
+
+  beforeEach(function () {
     spyOn(GmailAccountChecker, 'setTimeout_');
 
-    var req = new FakeXMLHttpRequest();
-    spyOn(GmailAccountChecker, 'makeXMLHttpRequest_').andReturn(req);
-    spyOn(req, 'open');
-    spyOn(req, 'send');
+    fakeReq = new FakeXMLHttpRequest();
+    spyOn(GmailAccountChecker, 'makeXMLHttpRequest_').andReturn(fakeReq);
+    spyOn(fakeReq, 'open');
+    spyOn(fakeReq, 'send');
 
-    var spy = jasmine.createSpy();
+    callbackSpy = jasmine.createSpy();
     spyOn(GmailAccountChecker, 'clearTimeout_');
+  });
 
-    var checker = new GmailAccountChecker(2, spy);
+  it('basic flow', function() {
+    var checker = new GmailAccountChecker(2, callbackSpy);
     expect(checker.index).toEqual(2);
     expect(checker.email).toEqual(null);
     expect(checker.unreadCount).toEqual(null);
@@ -24,12 +29,12 @@ describe('GmailAccountChecker', function () {
 
     expect(GmailAccountChecker.setTimeout_).toHaveBeenCalled();
     expect(GmailAccountChecker.makeXMLHttpRequest_).toHaveBeenCalled();
-    expect(req.open).toHaveBeenCalled();
-    expect(req.send).toHaveBeenCalled();
-    expect(spy).not.toHaveBeenCalled();
+    expect(fakeReq.open).toHaveBeenCalled();
+    expect(fakeReq.send).toHaveBeenCalled();
+    expect(callbackSpy).not.toHaveBeenCalled();
     expect(GmailAccountChecker.clearTimeout_).not.toHaveBeenCalled();
 
-    req.status = 200;
+    fakeReq.status = 200;
     var xml_text =
       '<feed xmlns="http://purl.org/atom/ns#" version="0.3">' +
       '<title>Gmail - Inbox for foo@bar.com</title>' +
@@ -40,32 +45,23 @@ describe('GmailAccountChecker', function () {
       '<modified>2010-11-02T03:35:17Z</modified>' +
       '</feed>';
     var xml = (new DOMParser()).parseFromString(xml_text, "text/xml");
-    req.responseXML = xml;
-    req.onload();
+    fakeReq.responseXML = xml;
+    fakeReq.onload();
+
     expect(checker.index).toEqual(2);
     expect(checker.email).toEqual('foo@bar.com');
     expect(checker.unreadCount).toEqual(300);
     expect(checker.lastUpdateTime).toNotEqual(null);
     expect(checker.lastError).toEqual(null);
 
-    expect(spy).toHaveBeenCalled();
+    expect(callbackSpy).toHaveBeenCalled();
     expect(GmailAccountChecker.clearTimeout_).toHaveBeenCalled();
     expect(GmailAccountChecker.setTimeout_.callCount).toEqual(2);
   });
 
   it('parse error', function() {
-    spyOn(GmailAccountChecker, 'setTimeout_');
-
-    var req = new FakeXMLHttpRequest();
-    spyOn(GmailAccountChecker, 'makeXMLHttpRequest_').andReturn(req);
-    spyOn(req, 'open');
-    spyOn(req, 'send');
-
-    var spy = jasmine.createSpy();
-    spyOn(GmailAccountChecker, 'clearTimeout_');
-
-    var checker = new GmailAccountChecker(2, spy);
-    expect(checker.index).toEqual(2);
+    var checker = new GmailAccountChecker(1, callbackSpy);
+    expect(checker.index).toEqual(1);
     expect(checker.email).toEqual(null);
     expect(checker.unreadCount).toEqual(null);
     expect(checker.lastUpdateTime).toEqual(null);
@@ -73,25 +69,25 @@ describe('GmailAccountChecker', function () {
 
     expect(GmailAccountChecker.setTimeout_).toHaveBeenCalled();
     expect(GmailAccountChecker.makeXMLHttpRequest_).toHaveBeenCalled();
-    expect(req.open).toHaveBeenCalled();
-    expect(req.send).toHaveBeenCalled();
-    expect(spy).not.toHaveBeenCalled();
+    expect(fakeReq.open).toHaveBeenCalled();
+    expect(fakeReq.send).toHaveBeenCalled();
+    expect(callbackSpy).not.toHaveBeenCalled();
     expect(GmailAccountChecker.clearTimeout_).not.toHaveBeenCalled();
 
-    req.status = 200;
+    fakeReq.status = 200;
     var xml_text =
       '<feed xmlns="http://purl.org/atom/ns#" version="0.3">' +
       '</feed>';
     var xml = (new DOMParser()).parseFromString(xml_text, "text/xml");
-    req.responseXML = xml;
-    req.onload();
-    expect(checker.index).toEqual(2);
+    fakeReq.responseXML = xml;
+    fakeReq.onload();
+    expect(checker.index).toEqual(1);
     expect(checker.email).toEqual(null);
     expect(checker.unreadCount).toEqual(null);
     expect(checker.lastUpdateTime).toNotEqual(null);
     expect(checker.lastError).toNotEqual(null);
 
-    expect(spy).toHaveBeenCalled();
+    expect(callbackSpy).toHaveBeenCalled();
     expect(GmailAccountChecker.clearTimeout_).toHaveBeenCalled();
     expect(GmailAccountChecker.setTimeout_.callCount).toEqual(2);
   });
