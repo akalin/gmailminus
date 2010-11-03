8876686d8b796c96fbf7eafe359ea5064e2438a3
Frederick Akalin <akalin@gmail.com>
2010-11-03 01:31:30 -0700

    added tests

--- GmailAccountCheckerTest.js
+++ GmailAccountCheckerTest.js
@@ -1,11 +1,12 @@
 describe('GmailAccountChecker', function () {
   it('initial state', function() {
+    spyOn(GmailAccountChecker, 'setTimeout_');
+
     var req = new XMLHttpRequest();
     spyOn(GmailAccountChecker, 'makeXMLHttpRequest_').andReturn(req);
     spyOn(req, 'open');
     spyOn(req, 'send');
 
-    spyOn(GmailAccountChecker, 'setTimeout_');
     spyOn(GmailAccountChecker, 'clearTimeout_');
 
     var mail_state = new GmailAccountChecker(2, function() {});
@@ -14,5 +15,10 @@ describe('GmailAccountChecker', function () {
     expect(mail_state.unreadCount).toEqual(null);
     expect(mail_state.lastUpdateTime).toEqual(null);
     expect(mail_state.lastError).toEqual(null);
+
+    expect(GmailAccountChecker.setTimeout_).toHaveBeenCalled();
+    expect(GmailAccountChecker.makeXMLHttpRequest_).toHaveBeenCalled();
+    expect(req.open).toHaveBeenCalled();
+    expect(req.send).toHaveBeenCalled();
   });
 });
