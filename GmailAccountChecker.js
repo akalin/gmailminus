af141044cc5589898f102c290384f090427b69c4
Frederick Akalin <akalin@gmail.com>
2010-11-03 00:21:17 -0700

    moved function up

--- GmailAccountChecker.js
+++ GmailAccountChecker.js
@@ -52,6 +52,52 @@ GmailAccountChecker.prototype.startCheck = function() {
   }
 }
 
+function gmailNSResolver(prefix) {
+  if(prefix == 'gmail') {
+    return 'http://purl.org/atom/ns#';
+  }
+}
+
+GmailAccountChecker.prototype.parse_feed = function(xml) {
+  var titleSet = xml.evaluate("/gmail:feed/gmail:title",
+      xml, gmailNSResolver, XPathResult.ANY_TYPE, null);
+  var titleNode = titleSet.iterateNext();
+  if (!titleNode) {
+    this.fail();
+    return;
+  }
+  title = titleNode.textContent;
+  var match = /^Gmail - Inbox for (\S+)$/.exec(title);
+  email = match[1];
+
+  var fullCountSet = xml.evaluate("/gmail:feed/gmail:fullcount",
+      xml, gmailNSResolver, XPathResult.ANY_TYPE, null);
+  var fullCountNode = fullCountSet.iterateNext();
+  if (!fullCountNode) {
+    this.fail();
+    return;
+  }
+  fullCount = parseInt(fullCountNode.textContent);
+  this.update(email, fullCount);
+}
+
+GmailAccountChecker.prototype.schedule = function() {
+  if (this.pendingRequestTimerId_) {
+    window.clearTimeout(this.pendingRequestTimerId_);
+  }
+  var pollIntervalMin = 1000 * 60;  // 1 minute
+  var pollIntervalMax = 1000 * 60 * 60;  // 1 hour
+  var randomness = Math.random() * 2;
+  var exponent = Math.pow(2, this.requestFailures_);
+  var delay = Math.min(randomness * pollIntervalMin * exponent,
+                       pollIntervalMax);
+  delay = Math.round(delay);
+
+  var self = this;
+  this.pendingRequestTimerId_ =
+    window.setTimeout(function() { self.startCheck(); }, delay);
+}
+
 GmailAccountChecker.prototype.get_gmail_url = function() {
   return "https://mail.google.com/mail/u/" + this.index + "/";
 }
@@ -121,4 +167,3 @@ GmailAccountChecker.prototype.schedule = function() {
   this.pendingRequestTimerId_ =
     window.setTimeout(function() { self.startCheck(); }, delay);
 }
-
